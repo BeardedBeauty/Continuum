80d91da733f8274fce8af31c9771206b0cf714ed
John Ochs <john.w.ochs0@gmail.com>
2020-02-06 14:55:09 -0600

    moving things in react around

---
+++ client/src/components/Logo/index.js
@@ -0,0 +1,13 @@
+import React from "./node_modules/react";
+
+function Logo() {
+    return (
+        <>
+            <div className="leaf1">m</div>
+            <div className="leaf2"></div>
+            <div className="leaf3"></div>
+        </>
+    )
+}
+
+export default Logo;
