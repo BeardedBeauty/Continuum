4bf0a326e194b34d1202e14c8caa1b201a1811a5
John Ochs <john.w.ochs0@gmail.com>
2020-02-19 16:03:53 -0600

    Man css is frustrating

--- client/src/App.js
+++ client/src/App.js
@@ -101,6 +101,7 @@ class App extends React.Component {
                     <Gallery />
                     <br />
                     <img className="aboutimg" src={about} alt="about" />
+                    <p className="bio">Marty has 40 years of experience in fine art, graphic design, commercial art, indoor and outdoor murals, illustration, sign painting, and sculpture.  In addition, she collaborates with her husband to create artistic leather work and designs. She earned an AA degree from the University of Minnesota specializing in art and design and has a broad and deep experience with an eclectic blend of visual art forms. This versatile artist loves to use her artistic skills to serve and support her community.</p>
                     <div className="pusher"></div>
                 </div>
                 <Footer />
