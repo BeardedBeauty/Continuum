85b536d67ce72489fede70801838804f6da9fcf7
John Ochs <john.w.ochs0@gmail.com>
2020-02-20 13:46:45 -0600

    somehow css is worse than our political system

--- client/src/App.js
+++ client/src/App.js
@@ -101,10 +101,10 @@ class App extends React.Component {
                         </div>
                         <Gallery />
                         <br />
-                        <div className="aboutImageHideBorder"></div>
+                        <div className="whatThe"></div>
                         <div className="cssSadness">
                             <div className="center" >
-                                <p className="bio">Marty has 40 years of experience in fine art, graphic design, commercial art, indoor and outdoor murals, illustration, sign painting, and sculpture.  In addition, she collaborates with her husband to create artistic leather work and designs. She earned an AA degree from the University of Minnesota specializing in art and design and has a broad and deep experience with an eclectic blend of visual art forms. This versatile artist loves to use her artistic skills to serve and support her community.</p>
+                                <h3 className="bio">Marty has 40 years of experience in fine art, graphic design, commercial art, indoor and outdoor murals, illustration, sign painting, and sculpture.  In addition, she collaborates with her husband to create artistic leather work and designs. She earned an AA degree from the University of Minnesota specializing in art and design and has a broad and deep experience with an eclectic blend of visual art forms. This versatile artist loves to use her artistic skills to serve and support her community.</h3>
                             </div>
                             <img className="aboutimg" src={about} alt="about" />
                             <Footer />
