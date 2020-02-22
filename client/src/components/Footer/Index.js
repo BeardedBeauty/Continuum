2c7544fe886a9f1c724814699cff065ff4b795bc
John Ochs <john.w.ochs0@gmail.com>
2020-02-21 19:26:19 -0600

    Completed footer…?

--- client/src/components/Footer/Index.js
+++ client/src/components/Footer/Index.js
@@ -1,9 +1,22 @@
 import React from "react";
 import "./style.css";
+import insta from "../../assets/Images/mail.png";
+import mail from "../../assets/Images/insta.png";
 
 function Footer(props) {
     return (
-        <div className="footerContainer">F O O T E R</div>
+        <div className="footerContainer">
+            <br /><br />
+            <h4>Based in Eden Prairie, Minnesota</h4>
+            <br />
+            <a href="mailto:marty_ochs@yahoo.com">
+                <img src={insta} alt="insta" className="webconnect" />
+            </a>
+            <a href="https://www.instagram.com/martys_arts_continuum/">
+                <img src={mail} alt="mail" className="webconnect" />
+            </a>
+            <h5>© John Ochs 2020</h5>
+        </div>
     )
 };
 
