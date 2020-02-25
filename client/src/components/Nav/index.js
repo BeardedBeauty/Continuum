1c85959b7f05616f9257fff766ff4267ca680246
John Ochs <john.w.ochs0@gmail.com>
2020-02-24 19:11:35 -0600

    completed Nav Links

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -13,16 +13,23 @@ import {
 function Nav(props) {
     return (
         <div className="gallerytitle">
-            {props.home && <Link to="/">g a l l e r i e s</Link>}
-            <a href="mailto:marty_ochs@yahoo.com">
-                <img src={insta} alt="insta" className="webconnect" />
-            </a>
-            <a href="https://www.instagram.com/martys_arts_continuum/">
-                <img src={mail} alt="mail" className="webconnect" />
-            </a>
-            {/* <a href="https://www.instagram.com/martys_arts_continuum/">
+            <div className="center">
+                <div className="navLinkContainer">
+                    <Link to="/about" className="navLink">a b o u t</Link>
+                    {props.home && <Link to="/" className="navLink">g a l l e r i e s</Link>}
+                </div>
+            </div>
+            <div className="iconContainer">
+                <a href="mailto:marty_ochs@yahoo.com">
+                    <img src={insta} alt="insta" className="webconnect2" />
+                </a>
+                <a href="https://www.instagram.com/martys_arts_continuum/">
+                    <img src={mail} alt="mail" className="webconnect2" />
+                </a>
+                {/* <a href="https://www.instagram.com/martys_arts_continuum/">
             <img src={link} alt="link" className="webconnect" />
         </a> */}
+            </div>
         </div>
     )
 };
---
+++ client/src/pages/About/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import about from "../../assets/Images/about.JPG";
+
+function About() {
+    return (
+        <>
+            <div className="cssSadness">
+                <img className="aboutimg" src={about} alt="about" />
+                <div className="center" >
+                    <div className="bio">Marty has 40 years of experience in fine art, graphic design, commercial art, indoor and outdoor murals, illustration, sign painting, and sculpture.  In addition, she collaborates with her husband to create artistic leather work and designs. She earned an AA degree from the University of Minnesota specializing in art and design and has a broad and deep experience with an eclectic blend of visual art forms. This versatile artist loves to use her artistic skills to serve and support her community.</div>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default About;
