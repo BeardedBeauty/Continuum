b90d0e9680122ffa68b08a53aff58a8fefa64a6b
John Ochs <john.w.ochs0@gmail.com>
2020-03-03 10:15:06 -0600

    cleaning code

--- client/src/components/Gallery/index.js
+++ client/src/components/Gallery/index.js
@@ -36,24 +36,12 @@ class Gallery extends React.Component {
         api.getImages().then(res => {
             let e = res.data;
             for (let w = 0; w < e.length; w++) {
-                if (e[w].arttype === "fineart") {
-                    this.state.images.fineart.push(e[w].url);
-                }
-                else if (e[w].arttype === "leather") {
-                    this.state.images.leather.push(e[w].url);
-                }
-                else if (e[w].arttype === "illustrations") {
-                    this.state.images.illust.push(e[w].url);
-                }
-                else if (e[w].arttype === "murals") {
-                    this.state.images.murals.push(e[w].url);
-                }
-                else if (e[w].arttype === "signs") {
-                    this.state.images.signs.push(e[w].url);
-                }
-                else if (e[w].arttype === "misc") {
-                    this.state.images.misc.push(e[w].url);
-                }
+                if (e[w].arttype === "fineart") { this.state.images.fineart.push(e[w]); }
+                else if (e[w].arttype === "leather") { this.state.images.leather.push(e[w].url); }
+                else if (e[w].arttype === "illustrations") { this.state.images.illust.push(e[w].url); }
+                else if (e[w].arttype === "murals") { this.state.images.murals.push(e[w].url); }
+                else if (e[w].arttype === "signs") { this.state.images.signs.push(e[w].url); }
+                else if (e[w].arttype === "misc") { this.state.images.misc.push(e[w].url); }
             }
         }).catch(err => console.log(err));
     };
@@ -70,7 +58,7 @@ class Gallery extends React.Component {
         });
     }
 
-    // disable = s => s ? this.setState({ galleryLink: s }) : this.setState({ galleryLink: s });
+    runstate = () => console.log(this.state);
 
     render() {
         return (
@@ -80,6 +68,7 @@ class Gallery extends React.Component {
                         home={this.state.galleryLink}
                         scroll={this.state.scroll}
                     />
+                    <button onClick={this.runstate()}>state</button>
                     <Switch>
                         <Route path="/" exact component={GalleryFolders} />
                         <Route path="/fineart"
